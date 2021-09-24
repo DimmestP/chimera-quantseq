4bad54d0bc193b0a76ed94eaf1fd5159fbc33625
Sam Haynes <dimmestp@gmail.com>
2021-09-24 14:39:47 +0100

    Fixes incorrect simulated read 2 files

--- code/find_polyA_site.sh
+++ code/find_polyA_site.sh
@@ -1,11 +1,10 @@
-rm /homes/sjhaynes/chimera-quantseq/data/output/sorted_bam/polyA_reads.bed
 
-for f in /homes/sjhaynes/chimera-quantseq/data/output/sorted_bam/QuantSeq/*/*.bam;
+for f in /homes/sjhaynes/chimera-quantseq/data/output/simulated_quantseq_pipeline_output/sorted_bam/A13/*.bam;
 do
 	echo $f;
 	sample_name=$(expr match $f '.*\([ABE][0-9]\+\)_')
         bedtools bamtobed -i $f |\
 	bedtools intersect -a stdin -b\
 	../data/input/Scer_ref_genome/construct_integrated_genome/construct_genome_gffs/${sample_name}_sample_longest_full_ORF_with_constructs.gff | \
-        grep -w - | grep  mCherry > /homes/sjhaynes/chimera-quantseq/data/output/sorted_bam/QuantSeq/${sample_name}/${sample_name}_polyA_reads.bed
+        grep -w - > /homes/sjhaynes/chimera-quantseq/data/output/simulated_quantseq_pipeline_output/sorted_bam/${sample_name}/${sample_name}_polyA_reads.bed
 done;
